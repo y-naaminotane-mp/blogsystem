510fc6f90b61ac44d649be4ec7e5bf9607137973
y-naaminotane-mp <y-naamino>
2014-05-06 11:53:56 +0900

    20140506-new
    データベース接続の方法、関数定義などのファイルを追加
    またカレンダーの表示をするために変更など

--- html/blog.php
+++ html/blog.php
@@ -9,6 +9,16 @@
 
 </head>
 <body>
+<?php
+//php初期情報の確認。はじめにincludeして現在の記事を更新する
+include "../env/env.inc";
+include "../env/dbh.inc";
+include "../env/func.inc";
+//ブログの記事を読み込み、配列に格納する
+$q = "";
+$q = "select * from kiji order by kiji_num";
+queryarray($q);
+?>
 <!----------------------------------------------------------
 ブログの下書きとか
 このページでは
@@ -47,7 +57,40 @@
 <!-----------------------------カテゴリー---------------------->
 <div>
 <p>テスト　カレンダー</p>
+<table>
+<tr>
+ <th>日</th>
+ <th>月</th>
+ <th>火</th>
+ <th>水</th>
+ <th>木</th>
+ <th>金</th>
+ <th>土</th>
+</tr>
+<?php
+//ここからphpを使ってのカレンダー取得
+$y = 2014;
+$m = 5;
+$d = 1;
 
+while(checkdate($m , $d , $y)){
+	echo "<td>$d</td>";
+	if(date("w" , mktime(0 , 0 , 0 , $m , $d , $y) == "6"){
+		echo "</tr>";
+		if(checkdate($m , $d+1 , $y)){
+			echo "<tr>";
+		}
+	}
+	$d++;
+
+}
+
+?>
+
+
+
+
+</table>
 
 </div>
 
@@ -71,12 +114,14 @@
 //ここから始まる記事内容
 
 
+//記事の要素数を数える
+$kiji_count = count($kiji_num);
 
-
-
-
-
-
+for($i = 0 ; $i < $kiji_count ; $i++){
+	echo "<h2>$kiji_title[$i]</h2>";
+	echo "<p>$kiji_date[$i]</p>";
+	echo "<p>$kiji[$i]" . "</p>";
+}
 
 
 ?>
